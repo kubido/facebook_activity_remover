26b8698acb5e19b54d7487f13cba3f7adc65e36b
Rifki Fauzi <kubido@gmail.com>
2015-07-22 23:17:33 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# faceboook_activiy_remover
+# Descriptions
 Painless way to remove your facebook activities
+
+# HOW TO
+- Go to your facebook activity log
+- Load facebook activities you watn to remove (eg: go to year 2010)
+- Open console in chrome/firefox
+- Paste all the script in remover.js
+- type "remover.run()"
+- done
+
+# TODO
+- create a plugins for chrome & firefox
+- create a bookmarklet
+
---
+++ remover.js
@@ -0,0 +1,55 @@
+window.facebookActivityRemover || (window.facebookActivityRemover = {});
+
+remover = facebookActivityRemover
+remover.elementIdentifier = {
+  actionContainer:    '_51m- vTop _5ep7',
+  activityDeleteForm: 'form[action*=\'/delete\']',
+  confirmationLink:   'a[ajaxify*=\'timeline/delete\']'
+}
+
+remover.run = function(){ 
+  remover.inititalizeElements();
+  remover.runIntervalDeleteJob();
+}
+
+
+remover.inititalizeElements = function() {
+  elements = document.getElementsByClassName(remover.elementIdentifier.actionContainer);
+  for(i = 0; i < elements.length; i++) { 
+    link = elements[i].children[0].children[1].children[0]
+    link.click()
+  }
+}
+
+remover.getDeleteActivityForm = function(){
+  forms = document.querySelectorAll(remover.elementIdentifier.activityDeleteForm);
+  return forms
+}
+
+remover.submitDeleteActivityForm = function() {
+  setTimeout(function () {
+    forms = remover.getDeleteActivityForm()
+    for(i = 0; i < forms.length; i++) { 
+      forms[i].children[3].children[1].click();
+      console.log(i+" status: delete form submitted")
+    }
+  }, 2000)
+}
+
+remover.runIntervalDeleteJob = function(){
+  var i = 0; deleteLinks = remover.deleteLinks();
+
+  var intervalId = setInterval(function () {
+      deleteLinks[i].click();
+      console.log(i+" status: link clicked")
+      i++;
+      if (i <= deleteLinks.length) {
+         remover.submitDeleteActivityForm(deleteLinks, i)
+      }
+   }, 2500)
+  if(i == deleteLinks.length){clearInterval(intervalId)}  
+}
+
+remover.deleteLinks = function(){
+  return document.querySelectorAll(remover.elementIdentifier.confirmationLink)  
+}
